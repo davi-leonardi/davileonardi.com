d58a4aab44b247d28c062eda2c6499679f781f3e
Davi Leonardi <davi.leonardi@hotmail.com>
2022-10-11 16:54:22 -0300

    oct11,2022

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -33,22 +33,38 @@ $("#btn-home").click(function () {
     window.scrollTo(0, 0);
 });
 
+//$("#btn-about").click(function () {
+//    window.scrollTo(0, 980);
+//});
+
 $("#btn-about").click(function () {
-    window.scrollTo(0, 980);
+    window.scrollTo(0, window.innerHeight * 1.05); 
 });
+
+//$("#btn-skills").click(function () {
+//    window.scrollTo(0, 1950);
+//});
 
 $("#btn-skills").click(function () {
-    window.scrollTo(0, 1950);
+    window.scrollTo(0, window.innerHeight * 2.08);
 });
+
+//$("#btn-portfolio").click(function () {
+//    window.scrollTo(0, 2930);
+//});
 
 $("#btn-portfolio").click(function () {
-    window.scrollTo(0, 2930);
+    window.scrollTo(0, window.innerHeight * 3.13);
 });
 
+//$("#btn-contact").click(function () {
+//    window.scrollTo(0, 3940);
+//});
+
 $("#btn-contact").click(function () {
-    window.scrollTo(0, 3940);
+    window.scrollTo(0, window.innerHeight * 4.18);
 });
 
 $("#contact-me-btn,#about-link").click(function () {
-    window.scrollTo(0, 3940);
+    window.scrollTo(0, window.innerHeight * 4.18);
 });
