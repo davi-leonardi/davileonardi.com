d011c6d31a8a3b918ba0d66cc598ea6ebd9cc608
Davi Leonardi <davi.leonardi@hotmail.com>
2022-10-24 14:30:51 -0300

    railway1.1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-
 var builder = WebApplication.CreateBuilder(args);
-
-//var kvUrl = builder.Configuration["KeyVaultConfig:KVUrl"];
-//var tenantId = builder.Configuration["KeyVaultConfig:TenantId"];
-//var clientId = builder.Configuration["KeyVaultConfig:ClientId"];
-//var clientSecret = builder.Configuration["KeyVaultConfig:ClientSecretId"];
-
-//var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-//var client = new SecretClient(new Uri(kvUrl), credential);
-
-//builder.Configuration.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
 
 // Add services to the container.
 builder.Services.AddRazorPages();
